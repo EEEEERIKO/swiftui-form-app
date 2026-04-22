4286ff1cdfc78e3f20597430bd98e0d69187b92e
ERIKO <erik.valenciac@unac.edu.co>
2026-04-22 15:16:21 -0500

    refactor: code upgraded with good practices and senior recomendations

--- swiftui-form/ContentView.swift
+++ swiftui-form/ContentView.swift
@@ -14,26 +14,32 @@ struct ContentView: View {
     @State private var birthDate = Date()
     @State private var shouldSendNewsletter = false
     @State private var numberOfLikes = 0
+    @State private var showAlert = false
+    
     var body: some View {
-        NavigationView{
+        NavigationStack{
             Form {
-                Section(header: Text("Personal Information")){
-                    TextField("First Name", text: $firstName)
-                    TextField("First Name", text: $lastName)
-                    DatePicker("BirthDate", selection: $birthDate, displayedComponents: .date)
+                Section("Personal Information"){
+                    TextField("First Name", text: $firstName).textContentType(.givenName)
+                    TextField("Last Name", text: $lastName).textContentType(.familyName)
+                    DatePicker("Birth Date", selection: $birthDate, displayedComponents: .date)
+                        .datePickerStyle(.compact)
                 }
                 
-                Section(header: Text("Actions")) {
+                Section("Actions") {
                     Toggle("Send Newsletter", isOn: $shouldSendNewsletter)
                         .toggleStyle(SwitchToggleStyle(tint: .green))
-                    Stepper("Number of likes", value: $numberOfLikes, in:0...100)
-                    Text("This video has  \(numberOfLikes) likes")
-                    Link("Terms of Service", destination: URL(string: "https://virtual.unac.edu.co")!)
+                    Stepper( value: $numberOfLikes, in:0...100){
+                        Text("Likes: \(numberOfLikes)")
+                    }
+                    if let url = URL(string: "https://virtual.unac.edu.co"){
+                        Link("Terms of Service", destination: url)
+                    }
                 }
             }
             .navigationTitle("Account")
             .toolbar {
-                ToolbarItemGroup(placement: .navigationBarTrailing) {
+                ToolbarItemGroup(placement: .topBarTrailing) {
                     Button{
                         hideKeyboard()
                     } label: {
@@ -42,14 +48,27 @@ struct ContentView: View {
                     Button("Save", action: saveUser)
                 }
             }
+            .alert("User Saved", isPresented: $showAlert) {Button("OK", role: .cancel) { }}
         }
         .tint(.blue)
     }
+    func saveUser(){
+        guard !firstName.isEmpty else {
+            print("Nombre inválido")
+            return
+        }
+        
+        guard !lastName.isEmpty else {
+            print("Apellidos inválidos")
+            return
+        }
+        print("User saved")
+        showAlert = true
+        
+    }
 }
 
-func saveUser(){
-    print("User saved")
-}
+
 
 #Preview {
     ContentView()
