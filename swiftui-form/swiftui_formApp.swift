b85e4615f5d41fba819f3f9b1d467d856bf002fb
ERIKO <erik.valenciac@unac.edu.co>
2026-04-22 13:44:14 -0500

    Initial Commit

---
+++ swiftui-form/swiftui_formApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_formApp.swift
+//  swiftui-form
+//
+//  Created by Erik Valencia Cardona on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_formApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
